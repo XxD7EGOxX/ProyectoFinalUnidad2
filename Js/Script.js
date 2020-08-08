7f8c10f2f503800dd01f8332d3fd0c6fc58db23e
XxD7EGOxX <diop.rojas@gmail.com>
2020-08-08 14:01:19 -0500

    Primer Commit

---
+++ Js/Script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+/* Script ReaccionMouse1 */
+
+    $(".ReaccionMouse1").mouseover(function(){
+
+        $(this).css("background", "Gray").css("color","white");
+    
+    });
+
+    $(".ReaccionMouse1").mouseout(function(){
+
+        $(this).css("background", "White").css("color","white");
+    
+    });
+
+});
